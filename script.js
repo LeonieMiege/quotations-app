6bccce224ca428d675556c80323713b6c2315ba6
Leonie Miege <leonie.miege@gmail.com>
2025-02-28 12:43:22 +0100

    step 5 dynamically displaying quotations

--- script.js
+++ script.js
@@ -7,7 +7,27 @@ function submitting() {
         let author = document.querySelector("#author").value;
         console.log(text);
         console.log(author);
+        addQuote(quote, author);
     })
+}
+
+function addQuote(quote, author) {
+    let textQuote = document.createElement("p");
+    let textAuthor = document.createElement("p");
+    
+    textQuote.textContent = quote;
+    textAuthor.textContent = author;
+
+    textQuote.classList.add("text");
+    textAuthor.classList.add("author");
+
+    let quoteList = document.createElement("div");
+    quoteList.classList.add("quote");
+    
+    quoteList.appendChild(textQuote);
+    quoteList.appendChild(textAuthor);
+
+    document.querySelector(".quote-list").appendChild(quoteList);
 }
 
 submitting();
